6fca72391771a78e0d76da9bdebf2eaf303455b8
Alexander <sanya.core@gmail.com>
2026-07-05 18:20:47 +0300

    docker-compose has been updated

--- config/settings.py
+++ config/settings.py
@@ -162,6 +162,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_content")
 
 AUTH_USER_MODEL = "users.User"
 
